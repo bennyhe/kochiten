55d1a258eecc24ca4e9b758a59c1d12c24cb37f5
bennyhe <hesiyuan009@gmail.com>
2023-12-28 21:56:06 +0800

    feat: 发布上线

---
+++ src/config/data.js
@@ -0,0 +1,148 @@
+const CardData = [{
+  cardnamejp: '持ち帰り仕事',
+  cardnamecn: '带回家中的工作',
+  back: {
+    cardnamejp: '全知の権能',
+    cardnamecn: '全知的权能'
+  }
+},
+{
+  cardnamejp: '秘密のドリンク',
+  cardnamecn: '秘密饮料',
+  back: {
+    cardnamejp: '私の居場所',
+    cardnamecn: '我的容身之所'
+  }
+},
+{
+  cardnamejp: '趣味の自分磨き',
+  cardnamecn: '在兴趣方面自我锻炼',
+  back: {
+    cardnamejp: '窮鼠猫を嚙む',
+    cardnamecn: '老鼠急了也会咬猫'
+  }
+},
+{
+  cardnamejp: '人材派遣の秘訣',
+  cardnamecn: '人才派遣的秘密',
+  back: {
+    cardnamejp: '彼ら彼女らの恩返し',
+    cardnamecn: '他们与她们的报恩'
+  }
+},
+{
+  cardnamejp: 'フレッシュマン',
+  cardnamecn: '新人',
+  back: {
+    cardnamejp: '常識人の崩壊',
+    cardnamecn: '正常人的崩坏'
+  }
+},
+{
+  cardnamejp: '未熟な人心掌握術',
+  cardnamecn: '掌握陌生人思想之术',
+  back: {
+    cardnamejp: '豊穣の権能',
+    cardnamecn: '丰收的权能'
+  }
+},
+{
+  cardnamejp: 'エリートコース',
+  cardnamecn: '精英路线',
+  back: {
+    cardnamejp: '唯我独尊',
+    cardnamecn: '唯我独尊'
+  }
+},
+{
+  cardnamejp: '威風堂々',
+  cardnamecn: '威风堂堂',
+  back: {
+    cardnamejp: '勝利の権能',
+    cardnamecn: '胜利的权能'
+  }
+},
+{
+  cardnamejp: '天は二物を与えず',
+  cardnamecn: '上天不会给予两次机会',
+  back: {
+    cardnamejp: '悔し涙と捨て台詞',
+    cardnamecn: '将后悔的眼泪抹去的发言'
+  }
+},
+{
+  cardnamejp: '下界のアイドル',
+  cardnamecn: '下界的偶像',
+  back: {
+    cardnamejp: '暴虐の少女神',
+    cardnamecn: '暴虐的少女神'
+  }
+},
+{
+  cardnamejp: 'ムードメーカー',
+  cardnamecn: '气氛制造者',
+  back: {
+    cardnamejp: '未来視の権能',
+    cardnamecn: '未来视的权能'
+  }
+},
+{
+  cardnamejp: '万里眼',
+  cardnamecn: '万里眼',
+  back: {
+    cardnamejp: '女神の目にも涙',
+    cardnamecn: '女神的眼睛也会流泪'
+  }
+},
+{
+  cardnamejp: '遊びのテクニック',
+  cardnamecn: '游戏的技巧',
+  back: {
+    cardnamejp: '涙の懇願',
+    cardnamecn: '哭诉请愿'
+  },
+  extra: '固有能力面效果为（神殿）1任意资源及+1GP，能力名直译应为“享乐的技巧”'
+},
+{
+  cardnamejp: '笑顔のおねだり',
+  cardnamecn: '请求笑颜',
+  back: {
+    cardnamejp: '夜の権能',
+    cardnamecn: '夜的权能'
+  }
+},
+{
+  cardnamejp: 'センシティブな指先',
+  cardnamecn: '优雅的指示',
+  back: {
+    cardnamejp: 'おたのしみの時間',
+    cardnamecn: '享受的时光'
+  }
+},
+{
+  cardnamejp: '親分肌',
+  cardnamecn: '亲切感',
+  back: {
+    cardnamejp: 'アイツの助力',
+    cardnamecn: '那家伙的帮助'
+  },
+  extra: '固有能力面的效果应为（转生房间）“不获得英雄时且污秽+1的话，获得1个绿或赤的灵魂”'
+},
+{
+  cardnamejp: '現場上がりの叩き上げ',
+  cardnamecn: '现场的指示',
+  back: {
+    cardnamejp: '夢のバンドデビュー',
+    cardnamecn: '梦想中的乐队出道'
+  },
+  extra: '固有能力面括号内的内容意为（污秽cost需照常支付），能力名直译应为"积累实地经验后升职的实力者"'
+},
+{
+  cardnamejp: 'アタシのダチ（友人）',
+  cardnamecn: '我自己的友人',
+  back: {
+    cardnamejp: '鼓動の権能',
+    cardnamecn: '鼓舞的权能'
+  }
+}]
+export default CardData
--- src/pages/CardList.vue
+++ src/pages/CardList.vue
@@ -1,9 +1,29 @@
 <script setup>
 import { ref } from 'vue'
+import CardData from '../config/data.js'
+const mark = txt => {
+  if (txt) {
+    return `**${txt}**`
+  }
+}
 </script>
 
 <template>
   <div class="page cur">
     <h2>こちら異世界転生局能力配对速查表</h2>
+    <div class="card-list">
+      <div
+        class="card-list__item"
+        v-for="(cardItem, cardKey) in CardData"
+        v-bind:key="cardKey"
+      >
+        <p class="namejp">{{ cardItem.cardnamejp }}</p>
+        <p class="namecn">{{ mark(cardItem.cardnamecn) }}</p>
+        <h3>背面</h3>
+        <p class="namejp name2">{{ cardItem.back.cardnamejp }}</p>
+        <p class="namecn name2">{{ mark(cardItem.back.cardnamecn) }}</p>
+        <p class="extra">{{ cardItem.extra }}</p>
+      </div>
+    </div>
   </div>
 </template>
